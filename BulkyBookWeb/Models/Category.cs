f3db58a5864d68477704503c48348fadbcabb9cd
Rolit Trivedi <rolit454@gmail.com>
2026-06-28 17:11:36 +0530

    Setting up database

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+    }
+}
